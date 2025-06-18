c029e84d8d4a1178dc63c2ceed47826c5f97a1c5
shuhao2013 <345777100@qq.com>
2025-06-18 11:42:28 +0800

    Update setup_model_weights.py

--- setup_model_weights.py
+++ setup_model_weights.py
@@ -1,13 +1,17 @@
 import os
-import gdown
+import urllib.request
 
+# 创建保存目录
 os.makedirs('./saved_models/u2net', exist_ok=True)
-os.makedirs('./saved_models/u2net_portrait', exist_ok=True)
 
-gdown.download('https://drive.google.com/uc?id=1ao1ovG1Qtx4b7EoskHXmi2E9rp5CHLcZ',
-    './saved_models/u2net/u2net.pth',
-    quiet=False)
+# 下载函数
+def download(url, save_path):
+    print(f"Downloading from {url}...")
+    urllib.request.urlretrieve(url, save_path)
+    print(f"Saved to {save_path}")
 
-gdown.download('https://drive.google.com/uc?id=1IG3HdpcRiDoWNookbncQjeaPN28t90yW',
-    './saved_models/u2net_portrait/u2net_portrait.pth',
-    quiet=False)
+# u2net.pth - 高清版，176MB，适用于毛发级抠图
+u2net_url = 'https://huggingface.co/luannd/u2net/resolve/main/u2net.pth'
+u2net_path = './saved_models/u2net/u2net.pth'
+
+download(u2net_url, u2net_path)
